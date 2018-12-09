43c1ad297092be18091786f977cf346eda5df3a8
Emilis Kiškis <emilis.kiskis@mif.stud.vu.lt>
2018-12-09 20:25:37 +0200

    Copy file

--- disasm.asm
+++ disasm.asm
@@ -6,21 +6,68 @@
     ofn db 13 dup (0)
     ifh dw ?
     ofh dw ?
+    in_buff db 512 dup (?)
+    in_buff_end dw ?
+    in_buff_length dw ?
+    READ_LENGTH dw 1024
+    out_buff db 512 dup (?)
+    out_buff_i dw 0
     ;Strings
     open_if_error_msg db "Couldn't open input file", 0Dh, 0Ah, 24h
     create_of_error_msg db "Couldn't create output file", 0Dh, 0Ah, 24h
+    help_msg db "Usage: disasm [input file] [output file]", 0Dh, 0Ah, 9, "/?: show this help text", 0Dh, 0Ah, 9, "input file: source executable to be disassembled", 0Dh, 0Ah, 9, "output file: .asm file with disassembled code", 0Dh, 0Ah, 24h
 
 .code
 start:
     mov ax, @data
     mov ds, ax
     xor ax, ax
+    jmp read_pars
+
+do_help:
+    mov ah, 09h
+    lea dx, help_msg
+    int 21h
+;clean_exit:
+    mov ax, 4C00h
+    int 21h
 
 read_pars:
     ;Logic: disasm [/?] [input file] [output file]
     xor ch, ch
     mov cl, [es:80h] ;par length
     cmp cl, 0
+    je do_help
+    dec cl
+    mov si, 82h
+    cmp word ptr [es:si], "?/"
+    je do_help
+    read_ifn:
+        lea di, ifn
+        read_ifn_loop:
+            mov dl, byte ptr [es:si]
+            cmp dl, " "
+            je end_read_ifn
+            mov [di], dl
+            inc si
+            inc di
+        loop read_ifn_loop
+    end_read_ifn:
+    cmp cl, 0
+    je open_if
+    inc si
+    read_ofn:
+        lea di, ofn
+        read_ofn_loop:
+            mov dl, byte ptr [es:si]
+            cmp dl, 0
+            je open_if
+            cmp dl, 0Dh
+            je open_if
+            mov [di], dl
+            inc si
+            inc di
+        loop read_ofn_loop
 
 open_if:
     mov ax, 3D00h
@@ -50,9 +97,82 @@ create_of_error:
     int 21h
 
 main_logic:
-    nop
+    lea si, in_buff
+    lea di, out_buff
+
+    main_loop:
+        mov dl, byte ptr [si]
+        ;mov current_symbol, dl
+        
+        ;Increase input buffer iterator (si) address and check for read and print req's
+        cont_main_loop:
+            inc si
+            cmp si, in_buff_end
+            jb skip_read
+            cmp in_buff_length, 512
+            jb exit_main_loop
+            call Read
+            cmp in_buff_length, 0
+            je exit_main_loop
+            skip_read:
+            cmp out_buff_i, 512
+            jb skip_print
+            call Print
+            skip_print:
+    jmp main_loop
+    exit_main_loop:
+    ;Flush buffers after exit
+    call Print
 
 clean_exit:
     mov ax, 4C00h
     int 21h
+
+proc Read
+    push ax
+    push bx
+    push cx
+    push dx
+    
+    mov ah, 3Fh
+    mov bx, ifh
+    mov cx, READ_LENGTH
+    lea dx, in_buff
+    int 21h
+
+    lea si, in_buff
+    mov in_buff_end, si
+    add in_buff_end, ax
+    mov in_buff_length, ax
+    
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+endp Read
+
+;Print current amount of characters from output buffer to output file, reset di to beginning
+proc Print
+    push ax
+    push bx
+    push cx
+    push dx
+    
+    mov ah, 40h
+    mov bx, ofh
+    mov cx, out_buff_i
+    lea dx, out_buff
+    int 21h
+
+    mov out_buff_i, 0
+    lea di, out_buff
+
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+endp Print
+
 end start
